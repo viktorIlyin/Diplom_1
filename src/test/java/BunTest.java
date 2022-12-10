2f7050fdf361ff33f444d401304825e7e205578a
Viktor Ilyin <whoisitl@yandex.ru>
2022-12-10 22:51:15 +0400

    Diplom_1_fix

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,45 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Bun;
-import praktikum.Burger;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BunTest {
-    private final String name;
-    private final float price;
 
-    public BunTest(String name, float price) {
-        this.name = name;
-        this.price = price;
-    }
+    private Bun bun;
 
-    @Parameterized.Parameters
-    public static Object[][] Buns() {
-        return new Object[][]{
-                {"", 0},
-                {"bun", 12},
-                {"bigbun", 1234},
-                {"superbigbun", 12345678},
-                {"!@#$%^&*(){:><>?+-=", -228},
-        };
+    @Before
+    public void setUp() {
+        bun = new Bun("black bun", 100);
     }
 
     @Test
-    public void SetBunReturnOurBunTests() {
-        Burger burger = new Burger();
-        burger.setBuns(new Bun(name, price));
-        List<String> expected = new ArrayList<>();
-        expected.add(name);
-        expected.add(String.valueOf(price));
-        List<String> actual = new ArrayList<>();
-        actual.add(burger.bun.getName());
-        actual.add(String.valueOf(burger.bun.getPrice()));
-        assertEquals(expected, actual);
+    public void bunGetNameTest() {
+        String actualName = bun.getName();
+        String expectedName = "black bun";
+        assertEquals(expectedName, actualName);
+
+    }
+
+    @Test
+    public void bunGetPriceTest() {
+        Float actualPrice = bun.getPrice();
+        Float expectedPrice = 100F;
+        assertEquals(expectedPrice, actualPrice);
     }
 }
